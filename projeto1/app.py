87a06d256a3b0ff35477cefe8f115aaf8cbbe99a
FamosoReverse <eduardob.i207@gmail.com>
2024-06-20 16:56:37 -0300

    Update app.py

--- projeto1/app.py
+++ projeto1/app.py
@@ -1,10 +1,23 @@
 
+# num = int(input('Digite um número: '))
+#print ('Analisando o valor {}, seu antecessor é {} e o #sucessor é {}'.format(num -1,num,num+1))
 
+#======================================================
 
-    
-        print("[1] olá qual seu nome? ")
-    nome = input('insira seu nome: ')
-    
-    print ("olá, {nome}!")
+#n = int(input('Digite um número: '))
+#d = n * 2 
+#t = n * 3
+#r = n ** (1/2)
 
-   
+#========================================================
+
+#print ('o dobro de {} vale {}'.format(n,d))
+#print ('o triplo de {} vale {}'.format(n,t))
+#print ('a raiz quadrada de {} vale {}'.format(n,r))
+
+#=========================================================
+
+n1 = float(input('Insira primeira nota:'))
+n2 = float(input('Insira segunda nota:'))
+media = (n1 + n2) / 2
+print('a média entre {} e {} é igual a {}'.format(n1,n2,media))
